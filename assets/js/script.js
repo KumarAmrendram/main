c2515bdec5587ca8222e53cfd6cff0eb0ddb6e92
kumarAmrendram <amrendramkumar@gmail.com>
2023-07-06 20:51:56 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var menuHolder = document.getElementById('menuHolder')
+var siteBrand = document.getElementById('siteBrand')
+function menuToggle() {
+    if (menuHolder.className === "drawMenu") menuHolder.className = ""
+    else menuHolder.className = "drawMenu"
+}
+if (window.innerWidth < 426) siteBrand.innerHTML = "Polytropic"
+window.onresize = function () {
+    if (window.innerWidth < 420) siteBrand.innerHTML = "Polytropic"
+    else siteBrand.innerHTML = "Polytropic HR | Consultancy"
+}
