3b91afcb1f2f3d3366b05e4d22122960b4bf98f1
kumarAmrendram <amrendram07@gmail.com>
2023-07-09 22:17:01 +0530

    - adds popup

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,25 @@ window.onresize = function () {
     if (window.innerWidth < 420) siteBrand.innerHTML = "Polytropic HR"
     else siteBrand.innerHTML = "Polytropic HR | Consultancy"
 }
+
+
+// popup 
+document.addEventListener("DOMContentLoaded", function() {
+    var popup = document.getElementById("popup");
+    var closeIcon = document.getElementById("close-icon");
+    var popupTitle = document.getElementById("popup-title");
+    var popupMessage = document.getElementById("popup-message");
+  
+    closeIcon.addEventListener("click", function() {
+      popup.style.display = "none";
+    });
+  
+    // Set the content of the popup
+    setTimeout(function() {
+      popupTitle.textContent = "New Opening";
+      popupMessage.textContent = "3 accountant needed. Apply now!.";
+      popup.style.display = "block";
+    }, 2000);
+  });
+  
+  
