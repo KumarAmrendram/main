966e7ceb29e9bbc576cbd3d59dfa9b274d11b188
kumarAmrendram <amrendram07@gmail.com>
2023-07-09 23:02:41 +0530

    - email and popup

---
+++ assets/js/popup.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var popup = document.getElementById("popup");
+    var closeIcon = document.getElementById("close-icon");
+    var popupTitle = document.getElementById("popup-title");
+    var popupMessage = document.getElementById("popup-message");
+  
+    closeIcon.addEventListener("click", function() {
+      popup.style.display = "none";
+    });
+  
+    // Check if the popup has been shown in the current session
+    var popupShown = sessionStorage.getItem("popupShown");
+    if (!popupShown) {
+      // Set the content of the popup
+      popupTitle.textContent = "New Job Opening!";
+      popupMessage.textContent = "We have exciting job opportunities available. Apply now!";
+  
+      // Show the popup after a delay of 2 seconds
+      setTimeout(function() {
+        popup.style.display = "block";
+      }, 2000);
+  
+      // Set the flag to indicate the popup has been shown in the current session
+      sessionStorage.setItem("popupShown", "true");
+    }
+  });
+  
--- assets/js/script.js
+++ assets/js/script.js
@@ -10,24 +10,7 @@ window.onresize = function () {
     else siteBrand.innerHTML = "Polytropic HR | Consultancy"
 }
 
+ 
 
-// popup 
-document.addEventListener("DOMContentLoaded", function() {
-    var popup = document.getElementById("popup");
-    var closeIcon = document.getElementById("close-icon");
-    var popupTitle = document.getElementById("popup-title");
-    var popupMessage = document.getElementById("popup-message");
-  
-    closeIcon.addEventListener("click", function() {
-      popup.style.display = "none";
-    });
-  
-    // Set the content of the popup
-    setTimeout(function() {
-      popupTitle.textContent = "New Opening";
-      popupMessage.textContent = "3 accountant needed. Apply now!.";
-      popup.style.display = "block";
-    }, 2000);
-  });
   
   
