9e06c88b5872cdb5ffd070933faff1cbd0c6810d
kumarAmrendram <amrendram07@gmail.com>
2023-07-11 09:45:35 +0530

    -initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,23 @@
-var menuHolder = document.getElementById('menuHolder')
-var siteBrand = document.getElementById('siteBrand')
+var menuHolder = document.getElementById("menuHolder");
+var siteBrand = document.getElementById("siteBrand");
 function menuToggle() {
-    if (menuHolder.className === "drawMenu") menuHolder.className = ""
-    else menuHolder.className = "drawMenu"
+  if (menuHolder.className === "drawMenu") menuHolder.className = "";
+  else menuHolder.className = "drawMenu";
 }
-if (window.innerWidth < 426) siteBrand.innerHTML = "Polytropic"
+if (window.innerWidth < 426) siteBrand.innerHTML = "Polytropic";
 window.onresize = function () {
-    if (window.innerWidth < 420) siteBrand.innerHTML = "Polytropic HR"
-    else siteBrand.innerHTML = "Polytropic HR | Consultancy"
-}
+  if (window.innerWidth < 420) siteBrand.innerHTML = "Polytropic HR";
+  else siteBrand.innerHTML = "Polytropic HR | Consultancy";
+};
 
- 
+//  from
 
-  
-  
+const myFrom = document.getElementById("contact");
+const inpFile = document.getElementById("inpFile");
+
+myFrom.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const endpoint = "upload.php";
+    const formData = new FormData();
+})
